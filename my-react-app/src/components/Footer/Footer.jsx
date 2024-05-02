819b12625868feea55a507cfab6a6dc467f123de
joaoveasey <jovepedro@gmail.com>
2024-05-02 11:14:22 -0300

    - criando o app react com o vite - criando os componentes Footer e Navbar
    (02/05)

---
+++ my-react-app/src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+    return (
+        <>
+            <footer className={styles.footer}>
+                <div>
+                    <h2>© Copyright Conexão Canina</h2>
+                    <h3>Desenvolvido por Grupo PETS</h3>
+                </div>
+                <div>
+                    <h3>Contato: (16) 99999-9999</h3>
+                    <h3>E-mail: contato.conexaocanina@gmail.com</h3>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
---
+++ my-react-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { NavLink, useNavigate } from 'react-router-dom'
+import styles from "./Navbar.module.css"
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+            <nav className={styles.navbar}>
+                <NavLink to="/" className={styles.brand}>
+                    Conexão Canina
+                </NavLink>
+                <ul className={styles.link_list}>
+                    <li>
+                        <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : null )}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/sobre" className={({ isActive }) => (isActive ? styles.active : null )}>
+                                Sobre
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dicas" className={({ isActive }) => (isActive ? styles.active : null )}>
+                                Dicas
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/duvidas" className={({ isActive }) => (isActive ? styles.active : null )}>
+                                Dúvidas
+                        </NavLink>
+                    </li>
+                    {!user && (
+                        <>
+                            <li>
+                                <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : null )}>
+                                        Entrar
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
+                    {user && (
+                        <>
+                        <li>
+                            <NavLink className={styles.logout} onClick={logout}>
+                                    Sair
+                            </NavLink>
+                        </li>
+                    </>
+                    )}
+                    
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
