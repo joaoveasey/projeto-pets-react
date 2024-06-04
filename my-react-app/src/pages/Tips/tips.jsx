2827387ae18cb02f492833765d6db871b48599fa
Pedro H. Garbim <133874619+pedrohgarbim@users.noreply.github.com>
2024-06-04 12:48:46 -0300

    Revert "criação dos arquivos jsx.css da pagina duvidas "

--- my-react-app/src/pages/Tips/tips.jsx
+++ my-react-app/src/pages/Tips/tips.jsx
@@ -10,71 +10,40 @@ function Tips() {
         <img src="/1dogdicas.jpg" alt="primeirodogdicas" style={{ padding: '30px 0' ,width: '550px', height: 'auto', display: 'block', margin: '0 auto' }}/>
       </div>
       <div className='tempocertodicas'>
-        <h2>Qual o momento ideal para levar seu cachorro para cruzar?</h2>
-        <p>Os veterinários recomendam que o cruzamento de fêmeas ocorra, no mínimo,
-           a partir do terceiro cio. Nessa fase, o corpo da fêmea já estará suficientemente
-            desenvolvido para suportar a gravidez e garantir o desenvolvimento saudável
-             dos filhotes. Cruzamentos realizados no primeiro ou segundo cio aumentam 
-             significativamente os riscos e complicações tanto para
-              a mãe quanto para os filhotes.
-              </p>
-        <p id='tempocertomachos'>No caso dos machos, a partir dos 18 meses eles já podem ser
-         férteis. No entanto, a maturidade física e mental completa pode levar até 2 anos.
-          Avalie o desenvolvimento individual do seu cão antes de cruzá-lo para garantir
-           que não haja nenhum malefício ao animal.
-          </p>
+        <h2>TEMPO CERTO</h2>
+        <p>Fêmeas: A gestação ideal ocorre após o terceiro cio, quando o corpo da cadela está maduro o suficiente 
+          para suportar a gravidez e garantir o desenvolvimento saudável dos filhotes. 
+          Cruzar no primeiro ou segundo cio aumenta os riscos de complicações para a mãe e os filhotes.</p>
+        <p id='tempocertomachos'>Machos: A partir dos 18 meses, os machos já podem ser férteis. No entanto, a maturidade física e mental completa 
+          pode levar até 2 anos. Avalie o desenvolvimento individual do seu cão antes de cruzar.</p>
       </div>
       <div className='fotodogdicas'>
         <img src="/2dogdicas.png" alt="segundodogdicas" style={{ padding: '30px 0' ,width: '550px', height: 'auto', display: 'block', margin: '0 auto' }}/>
       </div>
       <div className='parperfeitodicas'>
-        <h2>Qual o par perfeito para meu cachorro?</h2>
-        <p>Selecionar o par para o cruzamento do seu cachorro deve ser uma decisão
-           bem pensada para evitar problemas futuros. Apesar de parecer simples,
-            escolher o par ideal requer estudo, cuidado e atenção.
-          </p>
-        <p>O melhor para garantir a saúde da futura mamãe é que o macho seja, no máximo,
-           do mesmo tamanho que a fêmea. Isso porque, se o macho for maior que ela, 
-           os filhotes podem ficar muito grandes, o que tornaria o parto mais complicado
-           podendo causar até mesmo uma eventual cesárea e um desgaste maior da mãe
-           no período de amamentação.
-          Se o seu cachorro possui uma raça definida, a escolha pode ser um pouco mais 
-          direta, já que as opções se restringem a outros cachorros da mesma raça.
-          </p>
-        <p id='parperfeitoinstrucao'>Além disso, é imprescindível garantir que os cachorros 
-        selecionados para o cruzamento não tenham laços sanguíneos, ou seja, que não sejam
-         da mesma família. Embora cruzamentos entre cães da mesma linhagem sejam uma prática
-          comum entre criadores profissionais para aperfeiçoar características específicas,
-           esse processo é arriscado e pode resultar em doenças graves e intratáveis nos 
-           descendentes. Recomenda-se deixar esse tipo de cruzamento para 
-           profissionais experientes.
-         </p>
+        <h2>COMO ESCOLHER O PAR PERFEITO?</h2>
+        <p>Selecionar o par para o cruzamento do seu cachorro pode parecer simples à primeira vista,
+           mas é uma etapa que demanda cuidado e atenção.</p>
+        <p>Se o seu cachorro possui uma raça definida, a escolha pode ser um pouco mais direta, 
+          já que as opções se restringem a outros cachorros da mesma raça.
+          No entanto, o tamanho é um fator crucial a considerar ao selecionar o par de cachorros para o acasalamento.
+          Se o macho for significativamente maior que a fêmea,
+          pode haver complicações durante a gestação e o parto, uma vez que a fêmea talvez não tenha a estrutura 
+          adequada para gerar filhotes de porte maior.</p>
+        <p id='parperfeitoinstrucao'>Além disso, é imprescindível garantir que os cachorros selecionados para o cruzamento não sejam irmãos. 
+          Cruzamentos consanguíneos aumentam significativamente o risco de doenças genéticas nos filhotes, portanto, 
+          sempre priorize acasalamentos entre cachorros de linhagens diferentes.</p>
       </div>
       <div className='fotodogdicas'>
         <img src="/3dogdicas.png" alt="terceirodogdicas" style={{ padding: '30px 0' ,width: '550px', height: 'auto', display: 'block', margin: '0 auto' }}/>
       </div>
       <div className='aproximacaodicas'>
-        <h2>Como aproximar os cães e como saber se estão prontos para cruzar?</h2>
-        <p> A saúde do animal deve estar sempre em primeiro lugar, tanto para seu cão 
-          quanto para seu par. Portanto, é imprescindível levar os animais ao veterinário 
-          antes de qualquer cruzamento. As vacinas devem estar em dia, e a proteção
-           contra vermes e parasitas deve ser adequada. Algumas doenças e condições 
-           devem ser cuidadosamente avaliadas; se presentes, o cruzamento não deve ocorrer
-            para evitar a transmissão de problemas de saúde aos filhotes.
-        </p>
-        <p>O passo final da preparação é socializar o casal. É indicado que eles se 
-          conheçam bem, antes até do período de cio começar. A probabilidade de rejeição
-           diminui muito quando eles já estão acostumados um com o outro. O melhor é 
-           que essas socializações ocorram no local onde o cruzamento vai acontecer. O ideal
-            é que o cruzamento de cachorro aconteça na casa do macho,
-             pois ali ele se sente mais confortável, “dono do terreno”.
-              A fêmea também costuma ser menos agressiva e mais receptiva 
-              fora do seu ambiente. Se, por algum motivo, não for possível na casa do macho
-               e for decidido que a cruza vai acontecer no território dela, 
-               leve-o lá algumas vezes antes para ele conhecer o lugar e se habituar. 
-               A presença de outros machos pode complicar a situação, então deixe apenas
-                o macho escolhido perto da fêmea para evitar brigas. 
-          </p>
+        <h2>APROXIMAÇÃO</h2>
+        <p>Antes de aproximar os dois parceiros escolhidos para o cruzamento, é fundamental levá-los ao veterinário para um
+          check-up completo. Certifique-se de que ambos estejam saudáveis e com as vacinas em dia, 
+          garantindo assim uma gestação e ninhada saudável.</p>
+        <p>Contrariando a crença popular, não é necessário esperar o cio da fêmea para realizar essa etapa, 
+          promova encontros entre os dois para que possam se familiarizar e criar vínculos.</p>
         <p id='aproximacaoinstrucao'>Durante as primeiras aproximações, vá com calma. 
           Permita que os cachorros se cheirem e se reconheçam, 
           facilitando a aceitação mútua. Essa fase inicial é crucial para o sucesso do cruzamento.</p>
@@ -83,34 +52,21 @@ function Tips() {
         <img src="/4dogdicas.png" alt="quartodogdicas" style={{ padding: '30px 0' ,width: '550px', height: 'auto', display: 'block', margin: '0 auto' }}/>
       </div>
       <div className='cruzamentodicas'>
-        <h2> Como funciona o cio?</h2>
-        <p> O intervalo entre os cios em cadelas costuma variar entre 6 e 8 meses, 
-          com cada ciclo durando aproximadamente três semanas, ou cerca de 21 dias.
-           Esteja atento ao comportamento da sua cadela, pois os sinais do cio podem
-            variar individualmente. Os primeiros sinais do período fértil incluem 
-            desobediência, inquietação, aumento na frequência de urinar, além de sinais
-             mais evidentes como a vulva inchada e pequenos sangramentos. Nesse momento,
-              os machos já se sentem atraídos, mas a fêmea ainda não permite a cópula.
-         </p>
-          <p> As fêmeas deixam os machos se aproximarem para cruzar depois da primeira
-             semana. Logo, eles podem ficar juntos depois de 8 ou 9 dias que o cio começou.
-              Os sinais dessa fase incluem a fêmea oferecer a parte traseira para o macho
-               e levantar o bumbum colocando a cauda de lado.Talvez ela recuse a cópula
-                quando for colocada junto com o macho. Se isso acontecer,
-                 pode ser que ainda não esteja pronta para cruzar, então tente de novo
-                  no dia seguinte.           
-          </p>
-        <p id='cruzamentoinstrucao'>Os cachorros nao devem ficar a sós. É importante 
-        sempre estar por perto, portanto, não deixe os cães completamente sozinhos. 
-        Mantenha o ambiente tranquilo para que os animais se sintam à vontade, mas não deixe
-         de supervisionar.
-        </p>
+        <h2>CRUZAMENTO</h2>
+        <p>É essencial considerar o momento ideal para o cruzamento. Nos primeiros dias do cio da fêmea, durante o proestro, 
+          ela ainda não está fértil e, portanto, não estará receptiva ao macho.</p>
+        <p>A partir do nono dia do ciclo, aproximadamente, 
+          ocorre a ovulação, momento em que a fêmea está fértil e pronta para o acasalamento.
+          Durante o encontro, é recomendável deixar os dois animais juntos em um local seguro,
+          mantendo-se afastado para que possam se sentir à vontade.</p>
+        <p id='cruzamentoinstrucao'>No entanto, é importante não deixá-los sozinhos, 
+          pois a fêmea pode rejeitar o macho de forma agressiva, potencialmente resultando em confrontos.</p>
       </div>
       <div className='fotodogdicas'>
         <img src="/5dogdicas.png" alt="quintodogdicas" style={{ padding: '30px 0' ,width: '550px', height: 'auto', display: 'block', margin: '0 auto' }}/>
       </div>
       <div className='partodicas'>
-        <h2>Pré-natal e parto</h2>
+        <h2>PRÉ-NATAL E PARTO</h2>
         <p>No período pré-natal, é fundamental cuidar da mãe com consultas veterinárias regulares para monitorar 
           sua saúde e garantir uma dieta equilibrada.</p>
         <p>Durante o parto, crie um ambiente tranquilo e seguro.</p>
@@ -120,32 +76,11 @@ function Tips() {
       <div className='fotodogdicas'>
         <img src="/6dogdicas.avif" alt="sextodogdicas" style={{ padding: '30px 0' ,width: '550px', height: 'auto', display: 'block', margin: '0 auto' }}/>
       </div>
-      <div className='consideracoes'>
-        <h2>Considerações Finais</h2>
-        <p> O cruzamento de cachorro deve ser muito bem pensado. Antes de decidir cruzar,
-           pesquise pela sua região quanto você gastaria com tudo que será preciso:
-            check-up, exames gestacionais, o nascimento, inclusive o preço da cesariana,
-             caso seja necessário, alimentação especial, primeiras vacinas dos filhotes etc.
-              Tenha tudo anotado para estar preparado.
-          <br /><br />O desmame ocorre entre 30 e 45 dias depois do parto,
-           dependendo do porte dos papais. A combinação da guarda costuma ser que os
-            tutores do macho ficam com um dos filhotinhos, e os outros bebês 
-            ficam com os tutores da fêmea.
-            <br /><br />Não faça o cruzamento de cachorro em todos os cios. A gestação
-             é um processo muito desgastante e estressante, e a saúde do animal pode
-              ficar frágil se ele não tiver tempo para se recuperar.
-              <br /><br />Sempre tome cuidado com a aproximação de machos quando
-               sua cachorra entrar no cio. Se ela cruzar com mais de um é possível, 
-               sim, que a ninhada venha com filhotes de diferentes pais, 
-               de tamanhos, cores e pesos distintos.
-               <br /><br />Fique atento a possíveis problemas no parto, na
-                amamentação dos filhotes e qualquer outro sinal de doença.
-          </p>
-        <p id='consideracoesfin'>O ambiente de nascimento dos filhotes precisa ser limpo e livre
-         de doenças infecciosas. Tenha certeza de que nesse local não houve nenhum animal
-          com cinomose ou parvovirose nos últimos 2 anos, por exemplo. Os agentes dessas
-           doenças podem sobreviver no ambiente por longos períodos de tempo e causam doenças
-            fatais aos filhotes.</p>
+      <div className='localdicas'>
+        <h2>LOCAL IDEAL</h2>
+        <p>A escolha do local para o cruzamento é crucial para o sucesso do processo. <br /><br />Geralmente, é mais favorável realizar as aproximações e o acasalamento na residência do macho.
+          Isso se deve ao fato de o macho estar em seu próprio território, onde se sentirá mais confortável.</p>
+        <p id='localinstrucao'>Além disso, a fêmea, estando fora de seu ambiente, tende a ser menos agressiva durante o processo.</p>
       </div>
     </>
     
