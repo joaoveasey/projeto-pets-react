fea81d608d0a98af240f3cd15faca0af743ff8e0
EnzoGarbim <enzogarbim2004@gmail.com>
2024-06-07 10:44:41 -0300

    Criação do menu hambúrguer no Navbar

--- my-react-app/src/components/Navbar/Navbar.jsx
+++ my-react-app/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,35 @@
 import React, { useState } from "react";
-import { NavLink, useNavigate } from 'react-router-dom'
-import styles from './Navbar.module.css'
-import { userAuthentication } from '../../hooks/userAuthentication'
-import { useAuthValue } from '../../context/AuthContext'
-import logo from "./img/logo.png"
-import userPic from "./img/user.png"
-import poligono from "./img/PolygonOpened.png"
+import { NavLink, useNavigate } from 'react-router-dom';
+import styles from './Navbar.module.css';
+import { userAuthentication } from '../../hooks/userAuthentication';
+import { useAuthValue } from '../../context/AuthContext';
+import logo from "./img/logo.png";
+import userPic from "./img/user.png";
+import poligono from "./img/PolygonOpened.png";
 
 const Navbar = () => {
-    const { user } = useAuthValue()
-    const { logout } = userAuthentication()
-    const navigate = useNavigate()
+    const { user } = useAuthValue();
+    const { logout } = userAuthentication();
+    const navigate = useNavigate();
     const [drawerOpen, setDrawerOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleLogout = () => {
-        logout()
-        navigate('/entrar')
-    }
+        logout();
+        navigate('/entrar');
+    };
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
-        <>
-            <nav className={styles.navbar}>
-                <NavLink to="/" className={styles.brand}>
-                    <img src={logo} alt="logo" width="120px" height="110px" />
-                    <p>Conexão Canina</p>
-                </NavLink>
+        <header className={styles.header}>
+            <NavLink to="/" className={styles.brand}>
+                <img src={logo} alt="logo" width="120px" height="110px" />
+                <p>Conexão Canina</p>
+            </NavLink>
+            <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ""}`}>
                 <ul className={styles.link_list}>
                     <li className={styles.links}>
                         <NavLink to="/">
@@ -32,12 +37,12 @@ const Navbar = () => {
                         </NavLink>
                     </li>
                     <li className={styles.links}>
-                        <NavLink to="/sobre" >
+                        <NavLink to="/sobre">
                             Sobre
                         </NavLink>
                     </li>
                     <li className={styles.links}>
-                        <NavLink to="/dicas" >
+                        <NavLink to="/dicas">
                             Dicas
                         </NavLink>
                     </li>
@@ -48,16 +53,16 @@ const Navbar = () => {
                     </li>
                     {!user ? (
                         <li className={styles.links}>
-                            <NavLink to="/entrar" className={styles.entrarButton} activeClassName={styles.active}>
+                            <NavLink to="/entrar" className={styles.entrarButton}>
                                 Entrar
                             </NavLink>
                         </li>
                     ) : (
                         <li className={styles.links}>
                             <div className={styles.drawerToggle} onClick={() => setDrawerOpen(!drawerOpen)}>
-                                    <img src={poligono} width="20px" height="18px"/>
+                                <img src={poligono} width="20px" height="18px" />
                             </div>
-                            <NavLink to="/usuario" className={styles.usuario} activeClassName={styles.active}>
+                            <NavLink to="/usuario" className={styles.usuario}>
                                 <img src={userPic} alt="Usuário" width="80px" height="45px" />
                                 <p>{user.displayName || 'Usuário'}</p>
                             </NavLink>
@@ -70,11 +75,15 @@ const Navbar = () => {
                             )}
                         </li>
                     )}
-
                 </ul>
             </nav>
-        </>
-    )
-}
+            <div className={styles.burger} onClick={toggleMenu}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+        </header>
+    );
+};
 
-export default Navbar
+export default Navbar;
