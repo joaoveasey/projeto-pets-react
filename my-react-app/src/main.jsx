ca6ef2774d3ffd493c5ae20b4d8c7fa299bdbd17
NicolasCanonico0 <nicolas.undiciati@fatec.sp.gov.br>
2024-06-15 18:32:38 -0300

    Integração com Google Analytics

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/global.css'
 import App from "./App.jsx"
 
+import initializeGA from './Analytics';
+initializeGA();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
