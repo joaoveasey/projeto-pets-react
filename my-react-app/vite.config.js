8167d387f1287ead3ad97c3f4f9c660369743016
joaoveasey <jovepedro@gmail.com>
2024-06-17 18:16:25 -0300

    CI/ CD

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: '.',  // Diretório raiz do projeto
+  build: {
+    outDir: 'dist',  // Diretório de saída
+  },
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
