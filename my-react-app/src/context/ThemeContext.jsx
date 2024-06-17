ed90c14ec38297abb5dfd06c8f51494781003e34
NicolasCanonico0 <nicolas.undiciati@fatec.sp.gov.br>
2024-06-17 19:13:46 -0300

    Implementação Dark Mode

---
+++ my-react-app/src/context/ThemeContext.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setTheme(storedTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.body.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './styles/global.css'
-import App from "./App.jsx"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/global.css';
+import App from "./App.jsx";
+import './index.css';
+import { ThemeProvider } from '../src/context/ThemeContext.jsx';
 
 import initializeGA from './Analytics';
 initializeGA();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ThemeProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
     </React.StrictMode>
-)
+);
